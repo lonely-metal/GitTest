31ce4808ea23c5011e2944d8c9643dc31b75c444
butter <paul_butterfield@kyi.biglobe.ne.jp>
2020-10-07 12:34:33 +0900

    button座標変更

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(57, 54);
+            this.button1.Location = new System.Drawing.Point(55, 79);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
